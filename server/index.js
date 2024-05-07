2532d7ac7b2d290646071eda8618de22a3ffb1f0
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2024-05-07 18:22:09 +0700

    feat: add find cars page and make routes

--- server/index.js
+++ server/index.js
@@ -7,12 +7,12 @@ const host = 'localhost';
 const publicFolder = path.join(__dirname, '../public');
  
 const server = http.createServer((req, res) => {
-  const url = req.url;
+  const { url } = req;
 
   if (url === '/' || url === '/home') {
     readFileAndResponse(res, 'index.html')
-  } else if (url === '/cari') {
-    readFileAndResponse(res, 'index.html')
+  } else if (url === '/cars') {
+    readFileAndResponse(res, 'cars.html')
   } else {
     readFileAndResponse(res, url)
   }
@@ -23,19 +23,14 @@ const readFileAndResponse = (res, filename) => {
 
   fs.readFile(filePath, (err, content) => {
     if (err) {
-      if (err.code === 'ENOENT') {
-        res.statusCode = 404;
-        res.end('404 Not Found');
-      } else {
-        res.statusCode = 500;
-        res.end('500 Internal Server Error');
-      }
-    } else {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/html');
-      res.end(content);
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('404 Not Found');
     }
-  });
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(content);
+});
 }
 
 server.listen(port, host, () => {
